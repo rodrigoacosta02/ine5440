1715fc23a0ec34bcc07300d27c4cdefdd889f76d
Gilney N. Mathias <gilney_salvo@hotmail.com>
2017-09-26 22:16:26 +0000

    Arrumado o stride

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -7,14 +7,14 @@
 int main(){
     double inicio, fim, tempo;
 
-    uint32_t sizes[] = {1024*32, 1024*64, 1024*128, 1024*256, 1024*512, 8*1024*1024};
+    uint32_t sizes[] = {1024*32, 1024*64, 1024*128, 1024*256, 1024*512, 1*1024*1024};
     uint32_t nSizes = sizeof(sizes)/sizeof(uint32_t);
     uint32_t strides[] = {1, 4, 16, 1024, 2046, 64*1024, 128*1024};
     uint32_t nStrides = sizeof(strides)/sizeof(uint32_t);
-    uint32_t nRun = 128;
+    uint32_t nRun = 30;
 
     uint32_t *a, *b;
-    uint32_t size, stride, aux;
+    uint32_t size, stride;
 
     for(uint32_t s1 = 0; s1<nSizes; s1++){
         size = sizes[s1];
@@ -27,10 +27,9 @@ int main(){
             stride = strides[s2];
             tempo = 0;
             for(uint32_t i = 0; i<nRun; i++){
-                aux = stride+i;
                 mytime(&inicio);
                 for(uint32_t j = 0; j < size; j++)
-                    a[(j+aux)%size] = b[(j+aux)%size];
+                    a[i] = b[(j*stride)%size];
                 mytime(&fim);
                 tempo += (fim-inicio);
             }
